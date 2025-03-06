7b4b4d3c65ffb4255d36a8cc3b6442a696ba277c
AlessioNar <alessio.nardin@gmail.com>
2025-03-06 15:50:40 +0100

    Bumping to 0.2.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,8 +8,8 @@ import sys
 project = 'tulit'
 author = 'AlessioNar'
 
-release = '0.2.2'
-version = '0.2.2'
+release = '0.2.3'
+version = '0.2.3'
 
 # -- General configuration
 sys.path.insert(0, os.path.abspath('../../tulit'))

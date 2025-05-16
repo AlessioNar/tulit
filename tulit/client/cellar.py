ec13d10cd9a25b02ceb9c7b77bc67f964a654697
AlessioNar <alessio.nardin@gmail.com>
2025-05-16 17:58:49 +0200

    Incapsulated the SPARQL calling function in the CellarClient class

--- tulit/client/cellar.py
+++ tulit/client/cellar.py
@@ -1,17 +1,113 @@
 import logging
 import argparse
 import requests
-import json
 from tulit.client.client import Client
-from tulit.sparql import send_sparql_query
+from SPARQLWrapper import SPARQLWrapper, JSON, POST
 
 class CellarClient(Client):
     
     def __init__(self, download_dir, log_dir, proxies=None):
         super().__init__(download_dir, log_dir, proxies)
         self.endpoint = 'http://publications.europa.eu/resource/cellar/'
-   
+        self.sparql_endpoint = "http://publications.europa.eu/webapi/rdf/sparql"
+    
+    def send_sparql_query(self, sparql_query_filepath, celex=None):
+        """
+        Sends a SPARQL query to the EU SPARQL endpoint and stores the results in a JSON file.
 
+        Parameters
+        ----------
+        sparql_query_filepath : str
+            The path to the file containing the SPARQL query.
+        response_file : str
+            The path to the file where the results will be stored.
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        FileNotFoundError
+            If the SPARQL query file is not found.
+        Exception
+            If there is an error sending the query or storing the results.
+
+        Notes
+        -----
+        This function assumes that the SPARQL query file contains a valid SPARQL query.
+        The results are stored in JSON format.
+
+        """
+
+        # Open SPARQL QUERY and print it to screen
+        try:
+            with open(sparql_query_filepath, 'r') as file:
+                sparql_query = file.read()
+            
+            if celex is not None:
+                # Option 1: If you want to use format()
+                sparql_query = sparql_query.replace("{CELEX}", celex) 
+
+                # send query to cellar endpoint and retrieve results
+                results = self.get_results_table(sparql_query)
+
+            return results
+    
+        except FileNotFoundError as e:
+            print(f"Error: The file {sparql_query_filepath} was not found.")
+            raise e
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            raise e
+
+    def get_results_table(self, sparql_query):
+        """
+        Sends a SPARQL query to the EU SPARQL endpoint and returns the results as a JSON object.
+
+        Parameters
+        ----------
+        sparql_query : str
+            The SPARQL query as a string.
+
+        Returns
+        -------
+        dict
+            The results of the SPARQL query in JSON format.
+
+        Raises
+        ------
+        Exception
+            If there is an error sending the query or retrieving the results.
+
+        Notes
+        -----
+        This function uses the SPARQLWrapper library to send the query and retrieve the results.
+        The results are returned in JSON format.
+
+        """        
+
+        try:
+            # Create a SPARQLWrapper object with the endpoint URL
+            sparql = SPARQLWrapper(self.sparql_endpoint)
+
+            # Set the SPARQL query
+            sparql.setQuery(sparql_query)
+
+            # Set the query method to POST
+            sparql.setMethod(POST)
+
+            # Set the return format to JSON
+            sparql.setReturnFormat(JSON)
+
+            # Send the query and retrieve the results
+            results = sparql.query().convert()
+
+            return results
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            raise e
+    
     def fetch_content(self, url) -> requests.Response:
         """
         Send a GET request to download a file
@@ -174,14 +270,12 @@ def main():
         print('No valid format')
         return None
         
-    results = send_sparql_query(sparql_query_filepath=sparql_query, celex=args.celex)
+    results = client.send_sparql_query(sparql_query_filepath=sparql_query, celex=args.celex)
         
     documents = client.download(results, format=args.format)
     
     print(documents)
 
-    
-# Example usage
 if __name__ == "__main__":
     main()
     

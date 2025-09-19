941d9a0f4ca682347c98ef5a70c54f56f4b527e6
AlessioNar <alessio.nardin@gmail.com>
2025-09-19 08:43:44 +0200

    Bump version to 0.2.13 in configuration files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,8 +8,8 @@ import sys
 project = 'tulit'
 author = 'AlessioNar'
 
-release = '0.2.12'
-version = '0.2.12'
+release = '0.2.13'
+version = '0.2.13'
 
 # -- General configuration
 sys.path.insert(0, os.path.abspath('../../tulit'))

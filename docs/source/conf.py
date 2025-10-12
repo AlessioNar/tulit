6231c2c76922cc818fdb86ff471b9d776e8101ad
AlessioNar <alessio.nardin@gmail.com>
2025-10-12 12:11:26 +0200

    Bump version to 0.3.0 in configuration files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,8 +8,8 @@ import sys
 project = 'tulit'
 author = 'AlessioNar'
 
-release = '0.2.13'
-version = '0.2.13'
+release = '0.3.0'
+version = '0.3.0'
 
 # -- General configuration
 sys.path.insert(0, os.path.abspath('../../tulit'))
